70a728c2aa714c92effdd9707a44b9a3275a20c8
Osamu <usamabinf@gmail.com>
2022-05-25 10:43:46 +0500

    013 - Done with modal loading on the fly

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -5,10 +5,23 @@ import StickeyHeader from './modules/StickeyHeader'
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+new MobileMenu()
+new StickeyHeader()
+let modal
 
-let mobileMenu = new MobileMenu()
-
-let stickeyHeader = new StickeyHeader()
+document.querySelectorAll(".open-modal").forEach(el => {
+	el.addEventListener("click", e => {
+		e.preventDefault()
+		if (typeof modal == "undefined") {
+			import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+				modal = new x.default()
+			}).catch(() => console.log("There was a problem."))
+			setTimeout(() => modal.openModal(), 20)
+		} else {
+			module.openModal()
+		}
+	})
+})
 
 if (module.hot) {
 	module.hot.accept()
