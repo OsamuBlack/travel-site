11caf86a782870f608141ef57dbf3d70445fbbc1
Osamu <usamabinf@gmail.com>
2022-05-26 12:36:56 +0500

    016 - Html File updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizer = require('css-minimizer-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
 
 const postCssPlugins = [
     require('postcss-mixins'),
@@ -9,35 +14,47 @@ const postCssPlugins = [
     require('autoprefixer')
 ] 
 
+let cssConfig = {
+    test: /\.css$/i,
+    use: [
+        {
+            loader: 'css-loader',
+            options: {
+                url: false
+            }
+        }, 
+        {
+            loader: 'postcss-loader', 
+            options: {
+                postcssOptions: {
+                    plugins: postCssPlugins
+                }
+            }
+        }
+    ]
+}
+
+pages = fse.readdirSync('./app').filter(function(file) {
+    return file.endsWith('.html')
+}).map(function(page) {
+    return new HtmlWebpackPlugin ({
+        filename: page,
+        template: `./app/${page}`
+    })
+})
+
 let config = {
     entry: './app/assets/scripts/app.js',
+    plugins: pages,
     module: {
         rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    'style-loader', 
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false
-                        }
-                    }, 
-                    {
-                        loader: 'postcss-loader', 
-                        options: {
-                            postcssOptions: {
-                                plugins: postCssPlugins
-                            }
-                        }
-                    }
-                ]
-            }
+            cssConfig
         ]
     }
 }
 
 if (currentTask == 'dev') {
+    cssConfig.use.unshift('style-loader')
     config.output = {
         filename: "bundled.js",
         path: path.resolve(__dirname, 'app')
@@ -55,11 +72,22 @@ if (currentTask == 'dev') {
 }
 if (currentTask == 'built')
 {
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader)
     config.output = {
-        filename: "bundled.js",
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
         path: path.resolve(__dirname, 'dist')
     }
-    config.mode = 'production'
+    config.mode = 'production',
+    config.optimization = {
+        splitChunks: {chunks: 'all'},
+        minimize: true,
+        minimizer: [`...`, new CssMinimizer()]
+    }
+    config.plugins.push(
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'})
+    )
 }
 
 module.exports = config
